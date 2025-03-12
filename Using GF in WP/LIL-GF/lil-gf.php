e5074ea5b021390e8b30f3eab10d25bbcd5cb31f
mgrigoryan369 <martin.grigoryan.369@gmail.com>
2025-03-12 00:12:21 -0700

    Using GF in WP - Plugin initial version 1.0.0

---
+++ Using GF in WP/LIL-GF/lil-gf.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Plugin Name: LinkedIn Learning Gravity Forms
+ * Plugin URI: https://github.com/mgrigoryan369/
+ * Description: Simple plugin using hooks to modify GF
+ * Version: 1.0.0
+ * Author: Martin Grigoryan
+ * Author URI: https:/github.com/mgrigoryan369
+ * License: GPL-3.0+
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
+ */
+
+ 
