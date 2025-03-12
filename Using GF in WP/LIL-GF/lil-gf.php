c2fd08d5d13bcc95316d972bc8fbd4566a95b176
mgrigoryan369 <martin.grigoryan.369@gmail.com>
2025-03-12 05:11:40 -0700

    Using GF in WP - Added partial/save entry email notification functionality v1.0.3

--- Using GF in WP/LIL-GF/lil-gf.php
+++ Using GF in WP/LIL-GF/lil-gf.php
@@ -4,7 +4,7 @@
  * Plugin Name: LinkedIn Learning Gravity Forms
  * Plugin URI: https://github.com/mgrigoryan369
  * Description: Simple plugin using hooks to modify GF
- * Version: 1.0.2
+ * Version: 1.0.3
  * Author: Martin Grigoryan
  * Author URI: https:/github.com/mgrigoryan369
  * License: GPL-3.0+
@@ -17,7 +17,7 @@ if (!defined('WPINC')){
 }
 
 // Plugin version & URL constant
-define('LIL_GF_VERSION', '1.0.2');
+define('LIL_GF_VERSION', '1.0.3');
 define('LIL_GF_URL', plugin_dir_url(__FILE__));
 
 // Modify the default form label for 'First Name' in Gravity Forms (Form ID: 1)
@@ -52,6 +52,82 @@ function lil_gf_star_donors($entry, $form){
     }
 }
 
+/* Email notification on partial form submission/save with some extras
+*  There is an addon for this, but playing with the data was the purpose.*/
+function lil_gf_notify_admin($submission, $resume_token, $form, $entry) {
+    GFCommon::log_debug('LILGF Submission Contents: ' . print_r($submission, true));
+
+    $form_title = $form['title'];
+    $first_name = "Empty";
+    $last_name = "Empty";
+    $email = "Empty";
+    $phone = "Empty";
+    $partial = $submission['partial_entry'];
+
+    // Check if Form ID 2 - Job Application - then extract the Gold
+    if ($form['id'] === 2) {
+
+        // First Name (Field ID: 3.3)
+        if (!empty($partial['3.3'])) {
+            $first_name = $partial['3.3'];
+        }
+
+        // Last Name (Field ID: 3.6)
+        if (!empty($partial['3.6'])) {
+            $last_name = $partial['3.6'];
+        }
+
+        // Email (Field ID: 7)
+        if (!empty($partial['7'])) {
+            $email = $partial['7'];
+        }
+
+        // Phone (Field ID: 8)
+        if (!empty($partial['8'])) {
+            $phone = $partial['8'];
+        }
+    }
+
+    // Check if this is Form ID 5 - Survey - then extract the Gold
+    if ($form['id'] === 5) {
+
+        // First Name (Field ID: 3.3)
+        if (!empty($partial['3.3'])) {
+            $first_name = $partial['3.3'];
+        }
+
+        // Last Name (Field ID: 3.6)
+        if (!empty($partial['3.6'])) {
+            $last_name = $partial['3.6'];
+        }
+
+        // Email (Field ID: 5)
+        if (!empty($partial['5'])) {
+            $email = $partial['5'];
+        }
+
+        // Phone (Field ID: 6)
+        if (!empty($partial['6'])) {
+            $phone = $partial['6'];
+        }
+    }
+
+    //GFCommon::log_debug("LILGF Extracted Data - First Name: $first_name, Last Name: $last_name, Email: $email, Phone: $phone");
+
+    //Gather the details & compose the basic email 
+    $to = "martin.grigoryan.369@gmail.com";
+    $subject = "Partial Form Saved";
+    $message = "Guess what! Someone submitted/saved a partial form, and they don't know that we know!\n" .
+    "So perhaps we can warmly follow up after some days... that's if they left anything valuable.\n\n" .
+    "Form: $form_title\n\n" .
+    "First Name: $first_name\n" .
+    "Last Name: $last_name\n" .
+    "Email: $email\n" .
+    "Phone#: $phone";
+
+    wp_mail($to, $subject, $message);
+}
+
 
 // Hook: Modify the 'First Name' label in Gravity Forms
 add_filter('gform_name_first', 'lil_gf_change_first', 10, 2);
@@ -64,3 +140,6 @@ add_filter('gform_ip_address', '__return_empty_string');
 
 // Hook: Runs after a Gravity Forms submission to star the entry
 add_action('gform_after_submission', 'lil_gf_star_donors', 10, 2);
+
+// Hook: Notify admin when user saves progress
+add_action('gform_incomplete_submission_post_save', 'lil_gf_notify_admin', 10, 4);
