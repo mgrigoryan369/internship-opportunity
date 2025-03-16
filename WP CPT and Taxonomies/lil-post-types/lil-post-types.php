3d65dd16ccfa0c9e4ca1f97a878da05d343b737f
mgrigoryan369 <martin.grigoryan.369@gmail.com>
2025-03-16 14:38:42 -0700

    Initial CPT plugin

---
+++ WP CPT and Taxonomies/lil-post-types/lil-post-types.php
@@ -0,0 +1,15 @@
+<?php 
+/** 
+ * Plugin Name: Business Directory Post Types & Taxonomies
+ * Plugin URI: http://github.com/mgrigoryan369
+ * Description: A simple plugin for creating custom post types and taxonomies related to a business directory
+ * Version: 1.0.0
+ * Author: Martin Grigoryan
+ * Author URI: https://github.com/mgrigoryan369
+ * License: GPL-2.0+
+ * License UIR: http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain: lil-post-types
+ * Domain Path: /languages
+ */
+
+ 
