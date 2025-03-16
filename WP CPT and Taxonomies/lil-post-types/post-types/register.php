0b7e87f42d1bc45e4dbfef1580520f8bcb5bd6ba
mgrigoryan369 <martin.grigoryan.369@gmail.com>
2025-03-16 16:49:17 -0700

    Improve: Business CPT labels, fix syntax

--- WP CPT and Taxonomies/lil-post-types/post-types/register.php
+++ WP CPT and Taxonomies/lil-post-types/post-types/register.php
@@ -3,8 +3,15 @@
 // Business CPT
 function lil_register_business_type() {
     $labels = array(
-        'name' => __('Businesses, LILDOMAIN'), 
-        'singular_name' => __('Business'),
+        'name' => __('Businesses', LILDOMAIN), 
+        'singular_name' => __('Business', LILDOMAIN),
+        'featured_image' => __('Business Logo', LILDOMAIN),
+        'set_featured_image' => __('Set Business Logo', LILDOMAIN),
+        'remove_featured_image' => __('Remove Business Logo', LILDOMAIN),
+        'use_featured_image' => __('Use Business Logo', LILDOMAIN),
+        'archives' => __('Business Directory', LILDOMAIN),
+        'add_new' => __('Add New Business', LILDOMAIN),
+        'add_new_item' => __('Add New Business', LILDOMAIN),
     );
 
     $args = array(
