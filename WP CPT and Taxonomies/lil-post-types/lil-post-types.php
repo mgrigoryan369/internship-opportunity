46e7d5498562590e1ebf6b11cdce0cce954e61e8
mgrigoryan369 <martin.grigoryan.369@gmail.com>
2025-03-17 01:35:51 -0700

    Add: Flush rewrite rules upon activation | Bump to v1.0.5

--- WP CPT and Taxonomies/lil-post-types/lil-post-types.php
+++ WP CPT and Taxonomies/lil-post-types/lil-post-types.php
@@ -3,7 +3,7 @@
  * Plugin Name: Business Directory Post Types & Taxonomies
  * Plugin URI: http://github.com/mgrigoryan369
  * Description: A simple plugin for creating custom post types and taxonomies related to a business directory
- * Version: 1.0.4
+ * Version: 1.0.5
  * Author: Martin Grigoryan
  * Author URI: https://github.com/mgrigoryan369
  * License: GPL-2.0+
@@ -18,7 +18,7 @@ if (!defined('WPINC')){
 }
 
 //Define version, domain, and path
-define('LIL_VERSION', '1.0.4');
+define('LIL_VERSION', '1.0.5');
 define('LILDOMAIN', 'lil-post-types');
 define('LILPATH', plugin_dir_path(__FILE__));
 
@@ -32,3 +32,18 @@ add_action('init', 'lil_register_business_type');
 add_action('init', 'lil_register_event_type');
 add_action('init', 'lil_register_size_taxonomy');
 add_action('init', 'lil_register_location_taxonomy');
+
+//Register then flush rewrite rules upon plugin activation
+function lil_register_everything() {
+    lil_register_business_type();
+    lil_register_event_type();
+    lil_register_size_taxonomy();
+    lil_register_location_taxonomy();
+}
+
+function lil_rewrite_flush() {
+    lil_register_everything();
+    flush_rewrite_rules();
+}
+
+register_activation_hook(__FILE__, 'lil_rewrite_flush');
