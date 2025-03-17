9377d3b7a0b150c12a2ea207c1b1fd592d46368d
mgrigoryan369 <martin.grigoryan.369@gmail.com>
2025-03-17 00:22:38 -0700

    Add: Custom Taxonomy - Size | Bump to v1.0.3

--- WP CPT and Taxonomies/lil-post-types/lil-post-types.php
+++ WP CPT and Taxonomies/lil-post-types/lil-post-types.php
@@ -3,7 +3,7 @@
  * Plugin Name: Business Directory Post Types & Taxonomies
  * Plugin URI: http://github.com/mgrigoryan369
  * Description: A simple plugin for creating custom post types and taxonomies related to a business directory
- * Version: 1.0.2
+ * Version: 1.0.3
  * Author: Martin Grigoryan
  * Author URI: https://github.com/mgrigoryan369
  * License: GPL-2.0+
@@ -18,14 +18,16 @@ if (!defined('WPINC')){
 }
 
 //Define version, domain, and path
-define('LIL_VERSION', '1.0.2');
+define('LIL_VERSION', '1.0.3');
 define('LILDOMAIN', 'lil-post-types');
 define('LILPATH', plugin_dir_path(__FILE__));
 
 //Modules
 require_once(LILPATH . '/post-types/register.php');
+require_once(LILPATH . '/post-taxonomies/register.php');
 
 
 //Actions
 add_action('init', 'lil_register_business_type');
 add_action('init', 'lil_register_event_type');
+add_action('init', 'lil_register_size_taxonomy');
