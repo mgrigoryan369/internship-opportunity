420eb519b2a3ee5a56faa9f272031ea8946e60ef
mgrigoryan369 <martin.grigoryan.369@gmail.com>
2025-03-20 02:25:55 -0700

    Boilerplate for plugin

---
+++ WP Plugin Development/mg-plugin/mg-plugin.php
@@ -0,0 +1,17 @@
+<?php
+/* 
+Plugin Name: MG Plugin
+Description: Basic WP Plugin for LinkedIn Learning Course
+Plugin URI: https://github.com/mgrigoryan369
+Author: Martin Grigoryan
+Version: 1.0
+Text Domain: mg-plugin
+Domain Path: /languages
+License: GPLv2 or later
+License URI: https://www.gnu.org/licenses/gpls-2.0.txt
+*/
+
+// exit if file is called directly
+if (!defined('ABSPATH')){
+    exit;
+}
