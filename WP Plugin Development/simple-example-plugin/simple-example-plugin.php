9b4570de6f9d566bd2dacd1bbb38c9439ce77594
mgrigoryan369 <martin.grigoryan.369@gmail.com>
2025-03-19 19:22:31 -0700

    Initial example WP plugin

---
+++ WP Plugin Development/simple-example-plugin/simple-example-plugin.php
@@ -0,0 +1,25 @@
+<?php
+/* 
+Plugin Name: Simple Example Plugin
+Description: Welcome to WordPress plugin development.
+Plugin URI: https://github.com/mgrigoryan369
+Author: Martin Grigoryan
+Version: 1.0
+License: GPLv2 or later
+License URI: https://www.gnu.org/licenses/gpls-2.0.txt
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version
+2 of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+with this program. If not, visit: https://www.gnu.org/licenses/
+
+*/
+
