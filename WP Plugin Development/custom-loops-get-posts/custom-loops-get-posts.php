0f3b91228698ad83b78ab519ccc9783cd0f84450
mgrigoryan369 <martin.grigoryan.369@gmail.com>
2025-03-23 22:54:52 -0700

    Initial get_posts() plugin file

---
+++ WP Plugin Development/custom-loops-get-posts/custom-loops-get-posts.php
@@ -0,0 +1,16 @@
+<?php
+/*
+Plugin Name: Custom Loops: get_posts()
+Description: Demonstrates how to customize the WordPress Loop using get_posts().
+Plugin URI:  https://github.com/mgrigorayan369 
+Author:      Martin Grigoryan
+Version:     1.0
+*/
+
+// disable direct file access
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+
+
