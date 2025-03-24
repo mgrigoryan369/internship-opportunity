56c234d0eff2f5a2b93aa4629c0079fe255f3986
mgrigoryan369 <martin.grigoryan.369@gmail.com>
2025-03-23 23:14:45 -0700

    Add: functionality with shortcode

--- WP Plugin Development/custom-loops-get-posts/custom-loops-get-posts.php
+++ WP Plugin Development/custom-loops-get-posts/custom-loops-get-posts.php
@@ -12,5 +12,49 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
+// custom loop shortcode: [get_posts posts_per_page="" orderby=""]
+function custom_loop_shortcode_get_posts($atts){
 
+    // get global post variable
+    global $post;
 
+    // define shortcode variable
+    extract(shortcode_atts( array(
+        'posts_per_page' => 5,
+        'orderby' => 'date',
+    ), $atts));
+
+    // define get_post parameters 
+    $args = array(
+        'posts_per_page' => $posts_per_page, 
+        'orderby' => $orderby,
+    );
+
+    // get the posts
+    $posts = get_posts($args);
+
+    // begin output variable
+    $output = '<h3>Custom Loop: get_posts()</h3>';
+    $output .= '<ul>';
+
+    // loop 
+    foreach($posts as $post){
+        // prepare post data
+        setup_postdata($post);
+
+        // append each post data
+        $output .= '<li><a href="' . get_permalink() . '">' . get_the_title() . '</a></li>';
+    }
+
+    // reset query
+    wp_reset_postdata();
+
+    // append closing ul tag
+    $output .= '</ul>';
+
+    // return crafted output
+    return $output;
+}
+
+// register shortcode 
+add_shortcode('get_posts', 'custom_loop_shortcode_get_posts');
