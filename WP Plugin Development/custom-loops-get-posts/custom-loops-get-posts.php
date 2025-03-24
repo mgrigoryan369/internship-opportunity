8505683665560a5fb96d163101d288e72c27ca75
mgrigoryan369 <martin.grigoryan.369@gmail.com>
2025-03-24 00:27:49 -0700

    Fix: typo in plugin info

--- WP Plugin Development/custom-loops-get-posts/custom-loops-get-posts.php
+++ WP Plugin Development/custom-loops-get-posts/custom-loops-get-posts.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Custom Loops: get_posts()
 Description: Demonstrates how to customize the WordPress Loop using get_posts().
-Plugin URI:  https://github.com/mgrigorayan369 
+Plugin URI:  https://github.com/mgrigoryan369 
 Author:      Martin Grigoryan
 Version:     1.0
 */
