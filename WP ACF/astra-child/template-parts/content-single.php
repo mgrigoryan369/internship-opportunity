5e9dafd9effad764ba9bbdd3990c73312430d314
mgrigoryan369 <martin.grigoryan.369@gmail.com>
2025-03-25 13:19:33 -0700

    Add: display custom fields in template

--- WP ACF/astra-child/template-parts/content-single.php
+++ WP ACF/astra-child/template-parts/content-single.php
@@ -30,6 +30,35 @@
 
 	<?php astra_entry_content_single(); ?>
 
+    <?php 
+        if(get_field('location')){
+            echo '<p class="location">Location: ' . get_field('location') . "</p>";
+        }
+
+        if(get_field('profile_description')){
+            the_field('profile_description');
+        }
+
+        $friends = get_field('friends');
+        if ($friends){
+            echo '<h4 class="profile-friends-title">Friends</h4>';
+            echo '<ul class="profile-friends">';
+            $format = '<li><a href="%1$s" title="%2$s">%3$s</a></li>';
+        }
+
+        foreach($friends as $post) {
+            setup_postdata($post);
+            printf($format,
+                get_permalink(),
+                get_the_title(),
+                get_the_post_thumbnail(get_the_ID(), 'medium')
+            );
+        }
+
+        wp_reset_postdata();
+        echo '</ul>';
+	?>
+
 	<?php astra_entry_bottom(); ?>
 
 </article><!-- #post-## -->
