8da786d13392ad2473d0a887a92e340d10327e61
mgrigoryan369 <martin.grigoryan.369@gmail.com>
2025-03-27 01:14:53 -0700

    Initial plugin with styles and widget hook

---
+++ WP Actions and Filter Hooks/single-post-cta/single-post-cta.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+ * Plugin Name:       Single Post CTA
+ * Plugin URI:        https://github.com/mgrigoryan369
+ * Description:       Adds sidebar (widget area) to single posts
+ * Version:           1.0.0
+ * Author:            Martin Grigoryan
+ * Author URI:        https://github.com/mgrigoryan369
+ * License:           GPL v2+
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       spc
+ */
+
+ // disable direct file access
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+// load stylesheet
+function spc_load_stylesheet() {
+    if(is_single()){
+        wp_enqueue_style('spc-stylesheet', plugin_dir_url(__FILE__) . 'spc-styles.css');
+    }
+}
+
+// hook stylesheet
+add_action('wp_enqueue_scripts', 'spc_load_stylesheet');
+
+// register a custom sidebar
+function spc_register_sidebar() {
+    register_sidebar( array(
+	    'name'          => __( 'Single Post CTA', 'spc' ),
+	    'id'            => 'spc-sidebar',
+	    'description'   => __( 'Displays widget area on single posts', 'spc' ),
+	    'before_widget' => '<div class="widget spc">',
+	    'after_widget'  => '</div>',
+	    'before_title'  => '<h2 class="widgettitle spc-title">',
+	    'after_title'   => '</h2>',
+	) );
+}
+
+// hook sidebar
+add_action( 'widgets_init', 'spc_register_sidebar' );
