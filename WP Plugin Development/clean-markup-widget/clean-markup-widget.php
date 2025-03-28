7d7b5632cc9a51da1e9665a19bf93747fd2600fb
mgrigoryan369 <martin.grigoryan.369@gmail.com>
2025-03-28 02:07:36 -0700

    Add: widget form, update functionality, customize output

--- WP Plugin Development/clean-markup-widget/clean-markup-widget.php
+++ WP Plugin Development/clean-markup-widget/clean-markup-widget.php
@@ -28,6 +28,16 @@ class Clean_Markup_Widget extends WP_Widget {
 	
 	// output widget content
 	public function widget( $args, $instance ) {
+
+        // can also use 'extract($args)' to customize widget
+        extract( $args );
+
+        // add another class to the widget wrapper as demo
+        $custom_class = 'clean-markup-widget-another-class';
+        $before_widget = str_replace('class="', 'class="' . esc_attr($custom_class) . ' ', $before_widget);
+
+        // Output the widget
+	    echo $before_widget;
 		
 		// outputs the content of the widget
         $markup = '';
@@ -35,6 +45,8 @@ class Clean_Markup_Widget extends WP_Widget {
         if (isset($instance['markup'])){
             echo wp_kses_post($instance['markup']);
         }
+
+        echo $after_widget;
 		
 	}
 	
@@ -42,14 +54,35 @@ class Clean_Markup_Widget extends WP_Widget {
 	public function form( $instance ) {
 		
 		// outputs the widget form fields in the Admin Area
-		
-	}
+        $id = $this->get_field_id('markup');
+        $for = $this->get_field_id('markup');
+        $name = $this->get_field_name('markup');
+        $label = __('Markup/text:', 'custom-widget');
+        $markup = '<p>' . __('Clean markup.', 'custom-widget') . '</p>';
+
+        if(isset($instance['markup']) && !empty($instance['markup'])){
+            $markup = $instance['markup'];
+        }
+        ?>
+        <p>
+            <label for="<?php echo esc_attr($for); ?>"><?php echo esc_html($label); ?></label>
+            <textarea class="widefat" id="<?php echo esc_attr($id); ?>" name="<?php echo esc_attr($name); ?>"><?php esc_textarea($markup); ?></textarea>
+        </p>
+    
+    <?php }
 	
 	// process widget options
 	public function update( $new_instance, $old_instance ) {
 		
 		// processes the widget options
-		
+        $instance = array();
+
+        if (isset($new_instance['markup']) && !empty($new_instance['markup'])){
+            $instance['markup'] = $new_instance['markup'];
+        }
+
+        return $instance;
+
 	}
 	
 }
