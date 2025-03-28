1512e10ce6c7492c73dd136db179add56db0a075
mgrigoryan369 <martin.grigoryan.369@gmail.com>
2025-03-28 11:47:47 -0700

    Add: update user functionality, callback, admin notice

--- WP Plugin Development/user-management-mg/user-management-mg.php
+++ WP Plugin Development/user-management-mg/user-management-mg.php
@@ -95,8 +95,30 @@ function mgum_page_create_user() {
 }
 
 function mgum_page_update_user() {
-	// todo: Add update user form display
-	echo '<div class="wrap"><h1>Update User</h1></div>';
+	if ( ! current_user_can( 'manage_options' ) ) return;
+	?>
+	<div class="wrap">
+		<h1><?php echo esc_html( get_admin_page_title() ); ?></h1>
+		<form method="post">
+			<h3><?php esc_html_e('Update User', 'user-management-mg'); ?></h3>
+			<p>
+				<label for="email"><?php esc_html_e('Enter the user&rsquo;s email (required)', 'user-management-mg'); ?></label><br />
+				<input class="regular-text" type="text" size="40" name="email" id="email">
+			</p>
+			<p>
+				<label for="display-name"><?php esc_html_e('Enter a new Display Name for this user:', 'user-management-mg'); ?></label><br />
+				<input class="regular-text" type="text" size="40" name="display-name" id="display-name">
+			</p>
+			<p>
+				<label for="user-url"><?php esc_html_e('Enter a new Website URL for this user:', 'user-management-mg'); ?></label><br />
+				<input class="regular-text" type="text" size="40" name="user-url" id="user-url">
+			</p>
+
+			<input type="hidden" name="mgum_update_nonce" value="<?php echo wp_create_nonce('mgum_update_nonce'); ?>">
+			<input type="submit" class="button button-primary" value="<?php esc_html_e('Update User', 'user-management-mg'); ?>">
+		</form>
+	</div>
+	<?php
 }
 
 function mgum_page_delete_user() {
@@ -106,6 +128,7 @@ function mgum_page_delete_user() {
 
 // Form handlers — used to process form submissions
 
+// Create User
 add_action( 'admin_init', 'mgum_handle_create_user' );
 function mgum_handle_create_user() {
 	if (isset($_POST['mgum_nonce']) && wp_verify_nonce($_POST['mgum_nonce'], 'mgum_nonce')) {
@@ -143,11 +166,39 @@ function mgum_handle_create_user() {
 	}
 }
 
+// Update User
 add_action( 'admin_init', 'mgum_handle_update_user' );
 function mgum_handle_update_user() {
-	// todo: Process update user form POST data
+	if (isset($_POST['mgum_update_nonce']) && wp_verify_nonce($_POST['mgum_update_nonce'], 'mgum_update_nonce') ) {
+		
+		if (!current_user_can('manage_options')) wp_die();
+
+		$email = isset($_POST['email']) ? sanitize_email($_POST['email']) : '';
+		$display_name = isset($_POST['display-name']) ? sanitize_text_field($_POST['display-name']) : '';
+		$user_url = isset($_POST['user-url']) ? esc_url_raw($_POST['user-url']) : '';
+
+		$user_id = email_exists($email);
+		if (is_numeric( $user_id )) {
+			$userdata = array(
+				'ID' => $user_id,
+				'display_name' => $display_name,
+				'user_url' => $user_url
+			);
+			$user_id = wp_update_user($userdata);
+			if (is_wp_error($user_id)) {
+				$user_id = $user_id->get_error_message();
+			}
+		} else {
+			$user_id = __('User not found.', 'user-management-mg');
+		}
+
+		$redirect = admin_url('admin.php?page=mgum-update-user&result=' . urlencode($user_id));
+		wp_redirect($redirect);
+		exit;
+	}
 }
 
+// Delete User
 add_action( 'admin_init', 'mgum_handle_delete_user' );
 function mgum_handle_delete_user() {
 	// todo: Process delete user form POST data
@@ -159,11 +210,23 @@ add_action( 'admin_notices', 'mgum_admin_notices' );
 function mgum_admin_notices() {
 	$screen = get_current_screen();
 
+	// Adding User - admin msg
 	if ('user-management_page_mgum-create-user' === $screen->id && isset($_GET['result'])) {
 		$result = sanitize_text_field($_GET['result']);
 
 		if (is_numeric($result)) {
 			echo '<div class="notice notice-success is-dismissible"><p><strong>' . esc_html__('User added successfully.', 'user-management-mg') . '</strong></p></div>';
+		} else {
+			echo '<div class="notice notice-warning is-dismissible"><p><strong>' . esc_html($result) . '</strong></p></div>';
+		}
+	}
+
+	// Updating User - admin msg
+	if ('user-management_page_mgum-update-user' === $screen->id && isset($_GET['result'])) {
+		$result = sanitize_text_field($_GET['result']);
+	
+		if (is_numeric($result)) {
+			echo '<div class="notice notice-success is-dismissible"><p><strong>' . esc_html__('User updated successfully.', 'user-management-mg') . '</strong></p></div>';
 		} else {
 			echo '<div class="notice notice-warning is-dismissible"><p><strong>' . esc_html($result) . '</strong></p></div>';
 		}
