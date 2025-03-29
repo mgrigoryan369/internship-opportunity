2af66c0c47721a1bb097b6c10cee4ed313aa0643
mgrigoryan369 <martin.grigoryan.369@gmail.com>
2025-03-28 18:37:15 -0700

    Add: enqueue custom css on the mg-plugin admin settings page

--- WP Plugin Development/enqueue-examples/enqueue-examples.php
+++ WP Plugin Development/enqueue-examples/enqueue-examples.php
@@ -7,3 +7,18 @@ Author:      Martin Grigoryan
 Version:     1.0.0
 */
 
+
+// enqueue admin stylesheet on specific page
+add_action('admin_enqueue_scripts', 'mg_plugin_enqueue_style_admin_page');
+
+function mg_plugin_enqueue_style_admin_page($hook){
+    //wp_die($hook);
+
+    // This will conditionally target the admin settings page for the mg-plugin created earlier in this course
+    if('settings_page_mg_plugin' === $hook){
+        $src = plugin_dir_url(__FILE__) . 'admin/css/example-admin.css';
+        
+        wp_enqueue_style('mg-plugin-admin-page', $src, array(), null, 'all');
+    }
+}
+
