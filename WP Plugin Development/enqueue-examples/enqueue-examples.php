69d980211d73a64e0bfa5ab1b9b89c1be2669814
mgrigoryan369 <martin.grigoryan.369@gmail.com>
2025-03-28 18:05:51 -0700

    Initial plugin for enqueue examples

---
+++ WP Plugin Development/enqueue-examples/enqueue-examples.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Plugin Name: Enqueue Examples
+Description: More examples of JavaScript and CSS enqueue functions.
+Plugin URI:  https://github.com/mgrigoryan369
+Author:      Martin Grigoryan
+Version:     1.0.0
+*/
+
