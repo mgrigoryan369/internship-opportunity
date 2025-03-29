052c5fbc059dd17f0c333e3f679235c51a31f37c
mgrigoryan369 <martin.grigoryan.369@gmail.com>
2025-03-28 19:04:39 -0700

    Add: enqueue JS with dependency admin & public

--- WP Plugin Development/enqueue-examples/enqueue-examples.php
+++ WP Plugin Development/enqueue-examples/enqueue-examples.php
@@ -33,3 +33,25 @@ function mg_plugin_enqueue_style_admin_pages($hook){
         wp_enqueue_style('mg-plugin-admin-pages', $src, array(), null, 'all');
 	}
 }
+
+// enqueue with jQuery dependency example for specific admin page (settings page for mg plugin)
+add_action('admin_enqueue_scripts', 'mg_plugin_enqueue_jquery_admin');
+
+function mg_plugin_enqueue_jquery_admin($hook){
+
+    if ($hook !== 'settings_page_mg_plugin') return; // uncomment to load it everywhere in admin
+
+    $src = plugin_dir_url(__FILE__) . 'admin/js/example-admin.js';
+
+    wp_enqueue_script('mg-plugin-admin', $src, array('jquery'), '1.0');
+}
+
+// enqueue with jQuery dependency example for public pages
+add_action('wp_enqueue_scripts', 'mg_plugin_enqueue_jquery_public');
+
+function mg_plugin_enqueue_jquery_public(){
+
+    $src = plugin_dir_url(__FILE__) . 'public/js/example-public.js';
+
+    wp_enqueue_script('mg-plugin-public', $src, array('jquery'), '1.0');
+}
