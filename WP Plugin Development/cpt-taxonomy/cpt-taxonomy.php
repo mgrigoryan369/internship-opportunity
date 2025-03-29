d48ff25cd675f8e69defa0dc54e8b90c1915681a
mgrigoryan369 <martin.grigoryan.369@gmail.com>
2025-03-28 23:46:32 -0700

    Initial CPT plugin

---
+++ WP Plugin Development/cpt-taxonomy/cpt-taxonomy.php
@@ -0,0 +1,56 @@
+<?php
+/* 
+Plugin Name: CPT Taxonomy
+Description: Basic WP Plugin for CPT & Taxonomy
+Plugin URI: https://github.com/mgrigoryan369
+Author: Martin Grigoryan
+Version: 1.0
+Text Domain: mg-plugin
+License: GPLv2 or later
+License URI: https://www.gnu.org/licenses/gpls-2.0.txt
+*/
+
+// exit if file is called directly
+if (!defined('ABSPATH')){
+    exit;
+}
+
+
+// Add 'testimonial' custom post type
+function mg_plugin_add_custom_post_type_testimonial() {
+
+	$args = array(
+		'labels' => array(
+            'name' => 'Testimonials',
+            'singular_name' => 'Testimonial',
+            'add_new' => 'Add New',
+            'add_new_item' => 'Add New Testimonial',
+            'edit_item' => 'Edit Testimonial',
+            'new_item' => 'New Testimonial',
+            'view_item' => 'View Testimonial',
+            'search_items' => 'Search Testimonials',
+            'not_found' => 'No testimonials found',
+            'not_found_in_trash' => 'No testimonials found in Trash',
+            'all_items' => 'All Testimonials',
+            'menu_name' => 'Testimonials',
+        ),
+		'public' => true,
+		'publicly_queryable' => true,
+		'show_ui' => true,
+		'show_in_menu' => true,
+		'query_var' => true,
+		'rewrite' => array('slug' => 'testimonial'),
+		'capability_type' => 'post',
+		'has_archive' => true,
+		'hierarchical' => false,
+		'menu_position' => null,
+        'show_in_rest' => true,
+		'supports' => array('title', 'editor', 'author', 'thumbnail', 'excerpt', 'comments'),
+    );
+
+	register_post_type('testimonial', $args );
+
+}
+
+// Hook into init & fire the function 
+add_action( 'init', 'mg_plugin_add_custom_post_type_testimonial' );
